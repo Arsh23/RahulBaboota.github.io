43477255de6a1cc07517987fccbad68a5b12c796
RahulBaboota <rahulbaboota08@gmail.com>
2016-07-04 22:50:42 +0530

    Included static in HTML

---
+++ js/main.js
@@ -0,0 +1,63 @@
+//Function which gets the random quotes
+
+var getQuote = function()
+{
+	$.ajax(
+	{
+
+		url:'https://crossorigin.me/http://quotes.stormconsultancy.co.uk/random.json',
+		dataType: 'json',
+		type: 'GET',
+		xhrFields: 
+		{
+       		withCredentials: false,
+    	},
+
+		//If the request is succesful!!
+		success: function(data)
+		{	
+			//console.log(data);
+
+			// Adding the quote in the quote box
+			$('#qt-box').fadeOut(750, function()
+			{
+				$('#qt-box').html('"' + data.quote + '"').fadeIn(750);
+			})
+
+			// Adding the author name
+
+			if (data.author != '')
+			{
+				$('#author-box').fadeOut(750, function()
+				{
+					$('#author-box').html('-' + data.author).fadeIn(750);
+				})
+			}
+			else
+			{	
+				$('#author-box').fadeOut(750, function()
+				{
+					$('#author-box').html('-' + 'Unknown');
+
+				})
+			}
+		},
+
+		error: function()
+		{	
+			//console.log(data);
+
+			$('#qt-box').html("There was a problem with the API request");
+		}
+	});
+};
+
+//Document Ready Function
+
+$(document).ready(function()
+{
+	$('#button').on('click', function(){
+		getQuote();
+		// getQuote();
+	})
+});
