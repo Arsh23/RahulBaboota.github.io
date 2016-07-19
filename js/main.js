b365d9c50ba2516882b7ba7801daf65002b21a15
RahulBaboota <rahulbaboota08@gmail.com>
2016-07-19 23:21:13 +0530

    Added typewriter effect

---
+++ js/main.js
@@ -0,0 +1,128 @@
+// Function which adds typewriter effect to the text editor text
+
+var line2 = function()
+{
+	$('.red').typed(
+	{
+		strings: ["var define_rahul = function()"],
+		typeSpeed: 25,
+		onStringTyped: function()
+		{
+			line3();
+		}
+	});
+}
+
+var line3 = function()
+{
+	$('.purple1').typed(
+	{
+		strings: ["{"],
+		typeSpeed: 25,
+		onStringTyped: function()
+		{
+			line4();
+		}
+	});
+}
+
+var line4 = function()
+{
+	$('.green').typed(
+	{
+		strings: ["var definition = "],
+		typeSpeed: 25,
+		onStringTyped: function()
+		{
+			line5();
+		}
+	});
+}
+
+var line5 = function()
+{
+	$('.white1').typed(
+	{
+		strings: ["Hello There . People call me Rahul Baboota . I am a"],
+		typeSpeed: 25,
+		onStringTyped: function()
+		{
+			line6();
+		}
+	});
+}
+
+var line6 = function()
+{
+	$('.white2').typed(
+	{
+		strings: ["'Designer | Developer | Researcher | Aspiring Data Scientist | Star Gazer'"],
+		typeSpeed: 25,
+		onStringTyped: function()
+		{
+			line7();
+		}
+	});
+}
+
+var line7 = function()
+{
+	$('.yellow').typed(
+	{
+		strings: ["console.log()"],
+		typeSpeed: 25,
+		onStringTyped: function()
+		{
+			line8();
+		}
+	});
+}
+
+var line8 = function()
+{
+	$('.blue').typed(
+	{
+		strings: ["definition"],
+		typeSpeed: 25,
+		onStringTyped: function()
+		{
+			line9();
+		}
+	});
+}
+
+var line9 = function()
+{
+	$('.purple2').typed(
+	{
+		strings: ["}"],
+		typeSpeed: 25,
+		onStringTyped: function()
+		{
+			line10();
+		}
+	});
+}
+
+var line10 = function()
+{
+	$('.blue2').typed(
+	{
+		strings: ["define_rahul();"],
+		typeSpeed: 25,
+	});
+}
+var mydefinition = function()
+{
+	$('.gray').typed(
+	{
+		strings: ["/* Let's define a function which defines me */"],
+        typeSpeed: 25 , 
+        onStringTyped : function()
+        {
+        	line2();
+        }
+	});
+}
+
+mydefinition();
