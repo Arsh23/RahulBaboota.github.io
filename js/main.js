1fa35c2019a492703c8a07499f47ce0866dc3358
RahulBaboota <rahulbaboota08@gmail.com>
2016-08-16 22:24:51 +0530

    Structuring Page 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-// // /*-------------------------------------------------------------------------------------------------------------------------------*/
+// A sleep function in Javascript
+
+function Sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
+
+//*-------------------------------------------------------------------------------------------------------------------------------*/
 
 // Function which adds typewriter effect to the text editor text
 
@@ -126,9 +137,10 @@ var illuminateButton = function() {
 }
 
 var nextPageButtonClick = function() {
-    $('#next-page-button').click(function(){
-        $('#viewport1').css('transform','translateX(100%)');
-        $('#viewport2').css('transform','translateX(-100%)');
+    $('#next-page-button').click(function() {
+        $('#viewport1').css('transform', 'translateX(100%)');
+        $('#viewport1').css('transition', 'all 0.4s ease-in-out');
+        $('#viewport2').css('transform', 'translateX(-100%)');
         sublimeEntry();
     });
 }
@@ -143,104 +155,129 @@ nextPageButtonClick();
 
 // Sublime Box FadeIn Entry
 
-var sublimeEntry = function(){
+var sublimeEntry = function() {
     $('.sublime-viewport2').addClass('animated zoomIn');
     line1_viewport2();
 }
 
-// Changing the color of the background from white to hot pink
+// // Changing the color of the background 
 
-// var changeBackground = function(){
-//     $('#viewport2').fadeOut(750, function(){
-//         $('#viewport2').css('background','#1976D2');
-//         $('#viewport2').fadeIn(750);
-//     })
-// };
+var changeBackground = function() {
+    $('#viewport2').fadeOut(750, function() {
+        $('#viewport2').css('background', '#1976D2');
+        $('#viewport2').fadeIn(750);
+    })
+};
 
 // // Appearance Function for Travel Section !!
 
-// var appearTravel = function(){
-//     $('.box-ticket').css('opacity','100');
-//     $('.box-ticket').addClass('animated slideInLeft');  
-//     $('.plane').css('opacity','100');
-//     $('.plane').addClass('animated zoomIn');
+var appearTravel = function() {
+    $('.box-ticket').css('opacity', '100');
+    $('.box-ticket').addClass('animated slideInLeft');
+    $('.plane').css('opacity', '100');
+    $('.plane').addClass('animated zoomIn');
+    $('.suitcase').css('opacity', '100');
+    $('.suitcase').addClass('animated slideInLeft');
+}
 
-// }
+// Disappearance Function for Travel Section !! 
+
+var disappearTravel = function() {
+    $('.box-ticket').fadeOut(750);
+    $('.box-ticket').css('opacity','0');
+    $('.plane').fadeOut(750);
+    $('.plane').css('opacity','0');
+    $('.suitcase').fadeOut(750);
+    $('.suitcase').css('opacity','0');
+
+}
 
 // // Defining the Text to be written in the Sublime Box
 
-// var line1_viewport2 = function() {
-//     $('.gray-viewport2').typed({
-//         strings: ["## Let's paint the canvas with a little bit of Rahul .. Shall we !! "],
-//         typeSpeed: 25,
-//         startDelay: 1000,
-//         showCursor: false,
-//         onStringTyped: function() {
-//             line2_viewport2();
-//         }
-//     });
-// }
+var line1_viewport2 = function() {
+    $('.gray-viewport2').typed({
+        strings: ["## Let's paint the canvas with a little bit of Rahul .. Shall we !! "],
+        typeSpeed: 25,
+        startDelay: 1000,
+        showCursor: false,
+        onStringTyped: function() {
+            line2_viewport2();
+        }
+    });
+}
 
-// var line2_viewport2 = function() {
-//     $('.yellow-viewport2').typed({
-//         strings: ["def  "],
-//         typeSpeed: 25,
-//         startDelay: 0,
-//         showCursor: false,
-//         onStringTyped: function() {
-//             line3_viewport2();
-//         }
-//     });
-// }
+var line2_viewport2 = function() {
+    $('.yellow-viewport2').typed({
+        strings: ["def  "],
+        typeSpeed: 25,
+        startDelay: 0,
+        showCursor: false,
+        onStringTyped: function() {
+            line3_viewport2();
+        }
+    });
+}
 
-// var line3_viewport2 = function() {
-//     $('.blue-viewport2').typed({
-//         strings: ["about_rahul():"],
-//         typeSpeed: 25,
-//         startDelay: 0,
-//         showCursor: false,
-//         onStringTyped: function() {
-//             line4_viewport2();
-//         }
-//     });
-// }
+var line3_viewport2 = function() {
+    $('.blue-viewport2').typed({
+        strings: ["about_rahul():"],
+        typeSpeed: 25,
+        startDelay: 0,
+        showCursor: false,
+        onStringTyped: function() {
+            line4_viewport2();
+        }
+    });
+}
 
-// var line4_viewport2 = function() {
-//     $('.red-viewport2').typed({
-//         strings: ['about_rahul = '],
-//         typeSpeed: 25,
-//         startDelay: 0,
-//         showCursor: false,
-//         onStringTyped: function() {
-//             line5_viewport2();
-//         }
-//     })
-// }
+var line4_viewport2 = function() {
+    $('.red-viewport2').typed({
+        strings: ['about_rahul = '],
+        typeSpeed: 25,
+        startDelay: 0,
+        showCursor: false,
+        onStringTyped: function() {
+            line5_viewport2();
+        }
+    })
+}
 
-// var line5_viewport2 = function() {
-//     $('.white-viewport2').typed({
-//         strings: [' " Firstly , I like bright colors .. so !! " ^250 '],
-//         typeSpeed: 25,
-//         startDelay: 0,
-//         showCursor: false,
-//         onStringTyped: function() {
-//             changeBackground();
-//             line6_viewport2();
-//         }
-//     })
-// }
+var line5_viewport2 = function() {
+    $('.white-1-viewport2').typed({
+        strings: [' " Firstly , I like bright colors .. so !! " ^250 '],
+        typeSpeed: 25,
+        startDelay: 0,
+        showCursor: false,
+        onStringTyped: function() {
+            changeBackground();
+            line6_viewport2();
+        }
+    })
+}
 
-// var line6_viewport2 = function() {
-//     $('.white-viewport2').typed({
-//         strings: [' ^1000 " I love to travel and I intend to leave my footprint on all the 7 continents . " '],
-//         typeSpeed: 25,
-//         startDelay: 0,
-//         showCursor: false,
-//         onStringTyped: function() {
-//             appearTravel();
-//         }
-//     })
-// }
+var line6_viewport2 = function() {
+    $('.white-1-viewport2').typed({
+        strings: [' ^1000 " I love to travel and I intend to leave my footprint on all the 7 continents . " '],
+        typeSpeed: 25,
+        startDelay: 0,
+        showCursor: false,
+        onStringTyped: function() {
+            appearTravel();
+            line7_viewport2();
+        }
+    })
+}
 
-// /*-------------------------------------------------------------------------------------------------------------------------------*/
+var line7_viewport2 = function() {
+    $('.white-2-viewport2').typed({
+        strings: [' ^1000 " I love to hone my Penmenship Craft " '],
+        typeSpeed: 25,
+        startDelay: 0,
+        showCursor: false,
+        onStringTyped: function() {
+            disappearTravel();
+        }
+    })
+}
 
+/*-------------------------------------------------------------------------------------------------------------------------------*/
